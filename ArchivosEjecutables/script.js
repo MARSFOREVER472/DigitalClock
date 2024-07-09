97ec2c519e474fe67e2f44fd5bf511fb7037ccee
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-09 18:06:00 -0400

    Todos los archivos modificados!

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -1,3 +1,14 @@
+// PRIMERO LLAMAREMOS A LA FUNCIÓN PARA AÑADIR CEROS A CADA RANGO DE HORARIO Y FECHA...
+
+function addZero(x)
+{
+    if (x < 10)
+    {
+        x = '0' + x;
+    }
+    return x;
+}
+
 // LLAMAREMOS A LA FUNCIÓN PARA CONFIGURAR EL RELOJ MEDIANTE HORAS, MINUTOS Y SEGUNDOS MÁS LA FECHA ACTUAL...
 
 setInterval(function()
